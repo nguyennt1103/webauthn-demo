379bfd9e596cda2797f725b883dea18712ddcba5
nguyennt1103 <nguyen.nt1103@gmail.com>
2026-07-12 20:32:27 +0700

    Initial commit

---
+++ src/test/java/com/example/webauthndemo/WebauthnDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webauthndemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebauthnDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
